6e79df2e03345f78e15692e094d4f10bbaf61eae
Kian Dziak <kiandzk@gmail.com>
2022-09-28 11:16:33 -0500

    Cleaned up the methods to be more 'ruby like' Added a win checking method and logic to compare player guesses to the computers secert code.

--- ruby.rb
+++ ruby.rb
@@ -1,22 +1,46 @@
 COLORS = ['red', 'blue', 'green', 'purple', 'orange', 'cyan']
 
-  def secret_code
-    numbers = 4.times.map { Random.rand(7) }
-    colors = []
-    numbers.each do |number|
-      colors << COLORS[number - 1]
-    end
-    colors
+def secret_code_maker
+  numbers = 4.times.map { Random.rand(7) }
+  colors = []
+  numbers.each do |number|
+    colors << COLORS[number - 1]
   end
+  colors
+end
 
-  def player_guess
-    guess_array = []
-    while guess_array.length < 4
-      guess_array << gets.chomp
-    end
-    guess_array
+def player_guess_getter
+  guess_array = []
+  while guess_array.length < 4
+    guess_array << gets.chomp
   end
+  guess_array
+end
 
+def win_checker(computer_code, player_array)
+  return unless computer_code == player_array
 
-p secret_code
-p player_guess
+  p 'You Won!'
+end
+computer_code = secret_code_maker
+p computer_code
+player_array = player_guess_getter
+
+def color_checker_at_right_spot(computer_code, player_array)
+  i = 0
+  computer_code.each do |color|
+    if color == player_array[i]
+      puts '1 Black Peg' #this should increment a variable
+    elsif computer_code.include?(player_array[i])
+      puts '1 white peg'
+    else
+      p 'got nothing for ya'
+    end
+    i += 1
+  end
+end
+
+win_checker(computer_code, player_array)
+color_checker_at_right_spot(computer_code, player_array)
+
+p player_array
